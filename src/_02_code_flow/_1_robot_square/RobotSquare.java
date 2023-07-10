99bd0b3e3b91f7aa7586d764c919c4297db2179c
jereycui2008 <jeremycuid@gmail.com>
2023-07-10 11:41:49 -0700

    Update RobotSquare.java
    Robot Pen

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,28 +17,18 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
-
+    	rob.penDown();
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed(<speed>);    **/
- 
+    	rob.setSpeed(20);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
-    	
-
+    		
+    	int count = 0;
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move(<distance>);    **/
-  
-
-	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn(<angle>);    **/
-
-    	
-    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
-    		  /**     count += 1;    **/
-
-    	
-    	// Run the program. Did the Robot draw a line and turn?
+    	    	// Run the program. Did the Robot draw a line and turn?
     	 
     	
         // LOOP.  Make a while loop and move the DRAW, TURN, and INCREASE COUNT code 
@@ -51,6 +41,17 @@ public class RobotSquare {
     	 *            INCREASE COUNT
     	 *      }
     	 **/
+    	while (count < 4) {
+    		rob.move(280);
+
+    		// TURN.  Turn 90 degrees. Use this command:
+    	    		  /**     rob.turn(<angle>);    **/
+    	    	rob.turn(90);
+    	    	
+    	    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
+    	    		  /**     count += 1;    **/
+    	    	count += 1;
+    	}
     	
     	
     	// Run the program again. Did your Robot draw a square?
