c7b1ef89faec2459be4a38f4c100dad18a22f6af
jereycui2008 <jeremycuid@gmail.com>
2023-07-11 11:21:06 -0700

    Update WorldDomination.java
    World Domination

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,17 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String Response = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
+		if(Response == "yes") {
+			System.out.print("You will rule the world!");
+		}
+		else {
+			System.out.print("Sign up for classes at The League");
 
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+			
 	}
 }
 
